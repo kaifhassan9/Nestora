9520b2c88d2471a1bd5ef2c068b195e7414290a4
kaifhassan9 <mdkaif13551@gmail.com>
2026-07-13 20:21:17 +0530

    Added user authentication and signup functionality

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -16,7 +24,7 @@ main().then(()=>{
 });
 async function main(){
     await mongoose.connect(MONGO_URL);
-}
+};
 
 app.set("view engine" , "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -25,74 +33,52 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave:false,
+    saveUninitialized :true,
+    cookie:{
+        expires:Date.now() + 7*24*60*60*1000,
+        maxAge: 7*24*60*60*1000,
+        httpOnly:true,
+    },
+};
+
 app.get("/", (req,res) =>{
     res.send("hi , i am root");
 });
 
-//index route
-app.get("/listings" , async (req,res) =>{
-    const allListings = await Listing.find({});
-    res.render("listings/index" , {allListings});
-    });
+app.use(session(sessionOptions));
+app.use(flash());
 
-    //new route
-app.get("/listings/new", (req,res) =>{
-    res.render("listings/new.ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req,res,next)=>{
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
 });
 
-//show route
-app.get("/listings/:id" , async (req,res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-});
-//create route
-app.post("/listings", async (req, res) => {
- const newListing = new Listing(req.body.listing);
-    await newListing.save();
+app.use("/listings" , listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/" , userRouter);
 
-    res.redirect("/listings");
-});
-//edit route
-app.get("/listings/:id/edit" , async(req,res)=>{
-     let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
+
+app.use((req, res, next) =>{
+    next(new ExpressError(404,"page Not Found!"))
 });
 
-//update route
-app.put("/listings/:id", async (req,res)=>{
-    let {id} = req.params;
-   await Listing.findByIdAndUpdate(id,{...req.body.listing});
-   res.redirect(`/listings/${id}`);
+app.use((err,req,res,next)=>{
+ let {statusCode=500, message ="something went wrong!"} = err;
+       res.status(statusCode).render("error.ejs" ,{message});
+    // res.status(statusCode).send(message);
 });
-
-//delete route
-app.delete("/listings/:id", async (req,res)=>{
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-
-    res.redirect("/listings");
-});
-
-
-
-// app.get("/testListing", async (req,res) => {
-//     let sampleListing = new Listing({
-//         title:"My new villa",
-//         description:"by the beach",
-//         price:2000,
-//         location:"Calangute, Goa",
-//         country:"India",
-//     });
-
-    // await sampleListing.save();
-    // console.log("sample was saved");
-    // res.send("successfully testing");
-
-// });
-
-
 
 app.listen(8080 , () => {
     console.log("server is listening to port 8080");
